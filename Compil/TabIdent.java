a9d5149918dfe79dca9ca2781ce81820af722012
miuhaki <miurahareaki@gmail.com>
2014-02-14 09:09:13 +0100

    test

---
+++ Compil/TabIdent.java
@@ -0,0 +1,20 @@
+package Compil;
+
+import java.util.HashMap;
+
+public class TabIdent {
+	private HashMap<String,Ident> table;
+	
+	public TabIdent(int taille){
+		
+	}
+	/*public Ident chercherIdent(String clef){
+
+	}
+	public boolean existeIdent(String clef){
+
+	}
+	public void rangeIdent(String clef, Ident id){
+		
+	}*/
+}
