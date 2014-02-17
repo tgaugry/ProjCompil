eda41b1c625765bcc21dc6d3b2f84abde38fa779
miuhaki <miurahareaki@gmail.com>
2014-02-17 15:08:41 +0100

    declaation add

---
+++ Compil/Declaration.java
@@ -0,0 +1,16 @@
+
+public class Declaration {
+	public void declConst(String nom, int val) {
+		IdConst id = new IdConst(val);
+		table.rangeIdent (nom, val);
+	}
+	public void declConst(String nom, boolean val) {
+		table.rangeIdent (nom, val);
+	}
+	
+	public void declVar(String nom, int offset) {
+		tab
+		
+	}
+	
+}
