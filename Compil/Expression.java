5b28000dc5d7c76953603a763f35721f4d7dd902
miuhaki <miurahareaki@gmail.com>
2014-02-17 15:37:12 +0100

    Create Expression.java

---
+++ Compil/Expression.java
@@ -0,0 +1,18 @@
+package Compil;
+
+import java.util.Stack;
+
+public class Expression {
+	public enum optype {ARITH, LOGIQUE, ARLO }
+	private Stack<Character> operateurs;
+	private Stack<type>  operandes;
+	
+	public Expression (){
+		operateurs = new Stack<Character>();
+		operandes = new Stack<type>();
+		
+	}
+	public boolean evaluate {
+		
+	}
+}
