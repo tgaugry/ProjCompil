248fc768402751f1d4ae2d8e6460fa20cba3b992
Alexandre Audinot <alexandre.audinot@insa-rennes.fr>
2014-02-28 09:32:47 +0100

    Correction et generation d'une premiere version de la grammaire

--- Test/Test.java
+++ Test/Test.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import Compil.*;
 
