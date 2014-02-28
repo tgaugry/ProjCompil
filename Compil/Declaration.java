1b643ed27afc007af6a0eacf8a2a432095fff20c
unknown <aleonard@PC2-INFO1.educ.insa>
2014-02-28 08:54:19 +0100

    mise en commentaire de ce qui compile pas, pas d'autre avancee, juste pour pouvoir faire les tests unitaire

--- Compil/Declaration.java
+++ Compil/Declaration.java
@@ -1,17 +1,22 @@
 package Compil;
 
+import java.util.*;
+
+import Compil.Ident.Type;
+
 public class Declaration {
+	
 	public void declConst(String nom, int val) {
 		IdConst id = new IdConst(val);
-		table.rangeIdent (nom, id);
+		//table.rangeIdent (nom, id);
 	}
 	public void declConst(String nom, boolean val) {
 		IdConst id = new IdConst(val);
-		table.rangeIdent (nom, id);
+		//table.rangeIdent (nom, id);
 	}
 	
 	public void declVar(String nom) {
-		IdVar id = new IdVar();
-		table.rangeIdent (nom, id);
+		IdVar id = new IdVar(Type.BOOL);
+		//table.rangeIdent (nom, id);
 	}
 }
--- Compil/Expression.java
+++ Compil/Expression.java
@@ -5,14 +5,23 @@ import java.util.Stack;
 public class Expression {
 	public enum optype {ARITH, LOGIQUE, ARLO }
 	private Stack<Character> operateurs;
-	private Stack<type>  operandes;
+	private Stack<optype>  operandes;
 	
 	public Expression (){
 		operateurs = new Stack<Character>();
-		operandes = new Stack<type>();
+		operandes = new Stack<optype>();
 		
 	}
-	public boolean evaluate {
+	public boolean evaluate() {
+		optype operande;
+		if(operandes.empty()) {
+			if (operateurs.size() == 1) {return true;}
+			else {return false;}
+		}
+		else {
+			operande = operandes.pop();
+		}
+		return false;
 		
 	}
 }
