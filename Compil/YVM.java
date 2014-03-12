4bbf7b40daebb3a9cf18a714c8c7d5c4f5a2d72f
unknown <aleonard@PC1-INFO3.educ.insa>
2014-03-12 19:14:40 +0100

    Suppression du switch pour les operateurs, ajout de fonctions a la place

--- Compil/YVM.java
+++ Compil/YVM.java
@@ -8,83 +8,91 @@ public class YVM
 {	
 	public enum Op {ADD, SOUS, MUL, DIV, OU, ET, MOINS, NON, INF, INFEG, SUP, SUPEG, EG, DIF }
 	protected OutputStream output;
-	
+
 	public YVM(String nomFic)
 	{
 		output=Ecriture.ouvrir(nomFic);		
 	}
-	
+
 	public void debutProg() throws IOException{
 		Ecriture.ecrireStringln(output, "entete");
 	}
-	
+
 	public void ouvrePrinc() throws IOException{
 		Ecriture.ecrireString(output, "ouvrePrinc ");
 		Ecriture.ecrireInt(output, Yaka.tabIdent.compteVariables());
 		Ecriture.ecrireStringln("");
 	}
-	
+
 	public void lireConstOuVar(String nom) throws IOException{
 		Ident i = Yaka.tabIdent.chercherIdent(nom);
 		String texte = i.toString();
 		Ecriture.ecrireStringln(output, texte);
 	}
-	
+
 	public void lireImmediat(int i){
 		String texte = "iconst "+i;
 		Ecriture.ecrireStringln(output, texte);
 	}
-	
-	public void lireOperation(Op typeOp) throws IOException{
-		switch(typeOp)
-		{
-		case ADD:
-			Ecriture.ecrireStringln(output, "iadd");
-			break;
-		case SOUS:
-			Ecriture.ecrireStringln(output, "isub");
-			break;
-		case MUL:
-			Ecriture.ecrireStringln(output, "imul");
-			break;
-		case DIV:
-			Ecriture.ecrireStringln(output, "idiv");
-			break;
-		case OU:
-			Ecriture.ecrireStringln(output, "ior");
-			break;
-		case ET:
-			Ecriture.ecrireStringln(output, "iand");
-			break;
-		case MOINS:
-			Ecriture.ecrireStringln(output, "ineg");
-			break;
-		case NON:
-			Ecriture.ecrireStringln(output, "inot");
-			break;
-		case INF:
-			Ecriture.ecrireStringln(output, "iinf");
-			break;
-		case INFEG:
-			Ecriture.ecrireStringln(output, "iinfegal");
-			break;
-		case SUP:
-			Ecriture.ecrireStringln(output, "isup");
-			break;
-		case SUPEG:
-			Ecriture.ecrireStringln(output, "isupegal");
-			break;
-		case EG:
-			Ecriture.ecrireStringln(output, "iegal");
-			break;
-		case DIF:
-			Ecriture.ecrireStringln(output, "idiff");
-			break;
-		default:
-			System.out.println("Operation non reconnue :-(");
-			break;
-		}
+
+
+	public void lireAdd(){
+		Ecriture.ecrireStringln(output, "iadd");
 	}
+
+	public void lireSous(){
+		Ecriture.ecrireStringln(output, "isub");
+	}
+
+	public void lireMul(){
+		Ecriture.ecrireStringln(output, "imul");
+	}
+
+	public void lireDiv(){
+		Ecriture.ecrireStringln(output, "idiv");
+	}
+
+	public void lireOu(){
+		Ecriture.ecrireStringln(output, "ior");
+	}
+
+	public void lireEt(){
+		Ecriture.ecrireStringln(output, "iand");
+	}
+
+	public void lireNeg(){
+		Ecriture.ecrireStringln(output, "ineg");
+	}
+
+	public void lireNon(){
+		Ecriture.ecrireStringln(output, "inot");
+	}
+
+	public void lireInf(){
+		Ecriture.ecrireStringln(output, "iinf");
+	}
+
+	public void lireInfEg(){
+		Ecriture.ecrireStringln(output, "iinfegal");
+	}
+
+	public void lireSup(){
+		Ecriture.ecrireStringln(output, "isup");
+	}
+
+	public void lireSupEg(){
+		Ecriture.ecrireStringln(output, "isupegal");
+	}
+
+	public void lireEg(){
+		Ecriture.ecrireStringln(output, "iegal");
+	}
+
+	public void lireDiff(){
+		Ecriture.ecrireStringln(output, "idiff");
+	}
+
+
 
 	public void finProg() throws IOException{
 		Ecriture.ecrireStringln(output, "queue");
