3e4743ecf22cee3f2227ee70639e6be04369a120
GPrev <gabriel.prevosto@insa-rennes.fr>
2014-03-13 11:21:35 +0100

    Changement de la merde dans les conflits

--- Compil/YVM.java
+++ Compil/YVM.java
@@ -35,15 +35,11 @@ public class YVM
 		Ecriture.ecrireStringln(output, texte);
 	}
 
-
 	public void lireAdd(){
 		Ecriture.ecrireStringln(output, "iadd");
 	}
 
-<<<<<<< .minepublic YVM(String nomFic)
-{
-output=Ecriture.ouvrir(nomFic);	
-=======	public void lireSous(){
+	public void lireSous(){
 		Ecriture.ecrireStringln(output, "isub");
 	}
 
@@ -95,96 +91,8 @@ output=Ecriture.ouvrir(nomFic);
 		Ecriture.ecrireStringln(output, "idiff");
 	}
 
-
-
 	public void finProg() throws IOException{
 		Ecriture.ecrireStringln(output, "queue");
 		Ecriture.fermer(output);
 	}
->>>>>>> .theirs}
-
-public void debutProg() throws IOException{
-Ecriture.ecrireStringln(output, "entete");
-}
-
-public void ouvrePrinc() throws IOException{
-Ecriture.ecrireString(output, "ouvrePrinc ");
-Ecriture.ecrireInt(output, Yaka.tabIdent.compteVariables());
-Ecriture.ecrireStringln("");
-}
-
-public void lireConstOuVar(String nom) throws IOException{
-Ident i = Yaka.tabIdent.chercherIdent(nom);
-String texte = i.toString();
-Ecriture.ecrireStringln(output, texte);
-}
-
-public void lireImmediat(int i){
-String texte = "iconst "+i;
-Ecriture.ecrireStringln(output, texte);
-}
-
-
-public void lireAdd(){
-Ecriture.ecrireStringln(output, "iadd");
-}
-
-public void lireSous(){
-Ecriture.ecrireStringln(output, "isub");
-}
-
-public void lireMul(){
-Ecriture.ecrireStringln(output, "imul");
-}
-
-public void lireDiv(){
-Ecriture.ecrireStringln(output, "idiv");
-}
-
-public void lireOu(){
-Ecriture.ecrireStringln(output, "ior");
-}
-
-public void lireEt(){
-Ecriture.ecrireStringln(output, "iand");
-}
-
-public void lireNeg(){
-Ecriture.ecrireStringln(output, "ineg");
-}
-
-public void lireNon(){
-Ecriture.ecrireStringln(output, "inot");
-}
-
-public void lireInf(){
-Ecriture.ecrireStringln(output, "iinf");
-}
-
-public void lireInfEg(){
-Ecriture.ecrireStringln(output, "iinfegal");
-}
-
-public void lireSup(){
-Ecriture.ecrireStringln(output, "isup");
-}
-
-public void lireSupEg(){
-Ecriture.ecrireStringln(output, "isupegal");
-}
-
-public void lireEg(){
-Ecriture.ecrireStringln(output, "iegal");
-}
-
-public void lireDiff(){
-Ecriture.ecrireStringln(output, "idiff");
-}
-
-
-
-public void finProg() throws IOException{
-Ecriture.ecrireStringln(output, "queue");
-Ecriture.fermer(output);
-}
 }
