f695132a360ab4fc0ffbb8cb98f9ab856fc2df48
Alexandre Audinot <alexandre.audinot@insa-rennes.fr>
2014-03-13 21:16:51 +0100

    juste pour que git status arrete d'etre rouge

--- Utils/expr3.asm
+++ Utils/expr3.asm
@@ -1,166 +1,138 @@
-; entete 
-extrn lirent:proc, ecrent:proc, ecrbool:proc, ecrch:proc, ligsuiv:proc 
-.model SMALL 
-.586 
-
-.CODE 
-debut : 
-STARTUPCODE 
-
+extrn lirent:proc, ecrent:proc
+extrn ecrbool:proc
+extrn ecrch:proc, ligsuiv:proc
+; entete
+.model SMALL
+.586
+.CODE
+debut:
+STARTUPCODE
 
 ; ouvrePrinc 14
-mov bp, sp 
-sub sp, 14
-
+mov bp,sp
+sub sp,14
 
 ; ecrireChaine "c1="
-.DATA 
+.DATA
 mess0 DB "c1=$"
-.CODE 
+.CODE
 lea dx, mess0
-push dx 
-call ecrch 
-
+push dx
+call ecrch
 
 ; lireEnt -2
-lea dx,[bp-2] 
-push dx 
-call lirent 
+lea dx, [bp-2]
+push dx
+call lirent
 
-
-; aLaLigne 
-call ligsuiv 
-
+; aLaLigne
+call ligsuiv
 
 ; ecrireChaine "c3="
-.DATA 
+.DATA
 mess1 DB "c3=$"
-.CODE 
+.CODE
 lea dx, mess1
-push dx 
-call ecrch 
-
+push dx
+call ecrch
 
 ; lireEnt -6
-lea dx,[bp-6] 
-push dx 
-call lirent 
+lea dx, [bp-6]
+push dx
+call lirent
 
-
-; aLaLigne 
-call ligsuiv 
-
+; aLaLigne
+call ligsuiv
 
 ; ecrireChaine "c4="
-.DATA 
+.DATA
 mess2 DB "c4=$"
-.CODE 
+.CODE
 lea dx, mess2
-push dx 
-call ecrch 
-
+push dx
+call ecrch
 
 ; lireEnt -8
-lea dx,[bp-8] 
-push dx 
-call lirent 
+lea dx, [bp-8]
+push dx
+call lirent
 
-
-; aLaLigne 
-call ligsuiv 
-
+; aLaLigne
+call ligsuiv
 
 ; iload -6
-push word ptr [bp-6] 
-
+push word ptr[bp-6]
 
 ; iload -8
-push word ptr [bp-8] 
-
+push word ptr[bp-8]
 
 ; iconst 2
 push word ptr 2
 
+; idiv
+pop bx
+pop ax
+cwd
+idiv bx
+push ax
 
-; idiv 
-pop bx 
-pop ax 
-cwd 
-idiv bx 
-push ax 
-
-
-; iadd 
-pop bx 
-pop ax 
-add ax, bx 
-push ax 
-
+; iadd
+pop bx
+pop ax
+add ax,bx
+push ax
 
 ; iconst 5
 push word ptr 5
 
+; idiv
+pop bx
+pop ax
+cwd
+idiv bx
+push ax
 
-; idiv 
-pop bx 
-pop ax 
-cwd 
-idiv bx 
-push ax 
+; ecrireEnt
+call ecrent
 
-
-; ecrireEnt 
-call ecrent 
-
-
-; aLaLigne 
-call ligsuiv 
-
+; aLaLigne
+call ligsuiv
 
 ; iload -2
-push word ptr [bp-2] 
-
+push word ptr[bp-2]
 
 ; iconst 3
 push word ptr 3
 
-
 ; iload -2
-push word ptr [bp-2] 
+push word ptr[bp-2]
 
+; imul
+pop bx
+pop ax
+imul bx
+push ax
 
-; imul 
-pop bx 
-pop ax 
-imul bx 
-push ax 
-
-
-; iadd 
-pop bx 
-pop ax 
-add ax, bx 
-push ax 
-
+; iadd
+pop bx
+pop ax
+add ax,bx
+push ax
 
 ; iconst 10
 push word ptr 10
 
+; isub
+pop bx
+pop ax
+sub ax,bx
+push ax
 
-; isub 
-pop bx 
-pop ax 
-sub ax, bx 
-push ax 
+; ecrireEnt
+call ecrent
 
-
-; ecrireEnt 
-call ecrent 
-
-
-; queue 
-nop 
-EXITCODE 
-End debut 
-
+; queue
+nop
+EXITCODE
+End debut
 
