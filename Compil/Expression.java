ed6e5fa24c313e9b4a7bce38c142299f10a40867
unknown <aleonard@PC2-INFO2.educ.insa>
2014-03-13 11:27:11 +0100

    chgt yaka.jj

--- Compil/Expression.java
+++ Compil/Expression.java
@@ -6,81 +6,81 @@ import Compil.Ident.TypeVar;
 
 public class Expression {
 	public enum TypeOp {ARITH2, LOGIQUE2, ARITH1, LOGIQUE1, ARLO }
-	private Stack<TypeVar> operateurs;
-	private Stack<TypeOp>  operandes;
+	private Stack<TypeVar> operandes;
+	private Stack<TypeOp>  operateurs;
 	
 	public Expression (){
-		operateurs = new Stack<TypeVar>();
-		operandes = new Stack<TypeOp>();
+		operandes = new Stack<TypeVar>();
+		operateurs = new Stack<TypeOp>();
 	}
 	
 	public void addImmediate(TypeVar t) {
-		operateurs.push(t);
+		operandes.push(t);
 	}
 	
 	public void addOp(TypeOp t) {
-		operandes.push(t);
+		operateurs.push(t);
 	}
 	
 	public void addIdent(String identName) {
 		Ident i = Yaka.tabIdent.chercherIdent(identName);
-		operateurs.push(i.getType());
+		operandes.push(i.getType());
 	}
 	
 	public boolean evaluate() {
 		TypeOp operande;
 		TypeVar n1, n2;
-		if(operandes.empty()) {
-			if (operateurs.size() == 1) {return true;}
+		if(operateurs.empty()) {
+			if (operandes.size() == 1) {return true;}
 			else {return false;}
 		}
 		else {
-			operande = operandes.pop();
+			operande = operateurs.pop();
 			switch (operande) {
 			case ARITH1 :
-				if (operateurs.size() >= 1) {
-					n1 = operateurs.pop();
+				if (operandes.size() >= 1) {
+					n1 = operandes.pop();
 					if(n1 == TypeVar.INT){
-						operateurs.push(TypeVar.INT);
+						operandes.push(TypeVar.INT);
 						return evaluate();
 					}
 				}
 				break;
 			case ARITH2 :
-				if (operateurs.size() >= 2) {
-					n1 = operateurs.pop();
-					n2 = operateurs.pop();
+				if (operandes.size() >= 2) {
+					n1 = operandes.pop();
+					n2 = operandes.pop();
 					if(n1 == TypeVar.INT && n2 == TypeVar.INT){
-						operateurs.push(TypeVar.INT);
+						operandes.push(TypeVar.INT);
 						return evaluate();
 					}
 				}
 				break;
 			case LOGIQUE1 :
-				if (operateurs.size() >= 1) {
-					n1 = operateurs.pop();
+				if (operandes.size() >= 1) {
+					n1 = operandes.pop();
 					if(n1 == TypeVar.BOOL){
-						operateurs.push(TypeVar.BOOL);
+						operandes.push(TypeVar.BOOL);
 						return evaluate();
 					}
 				}
 				break;
 			case LOGIQUE2 :
-				if (operateurs.size() >= 2) {
-					n1 = operateurs.pop();
-					n2 = operateurs.pop();
+				if (operandes.size() >= 2) {
+					n1 = operandes.pop();
+					n2 = operandes.pop();
 					if(n1 == TypeVar.BOOL && n2 == TypeVar.BOOL){
-						operateurs.push(TypeVar.BOOL);
+						operandes.push(TypeVar.BOOL);
 						return evaluate();
 					}
 				}
 				break;
 			case ARLO :
-				if (operateurs.size() >= 2) {
-					n1 = operateurs.pop();
-					n2 = operateurs.pop();
+				if (operandes.size() >= 2) {
+					n1 = operandes.pop();
+					n2 = operandes.pop();
 					if(n1 == TypeVar.INT && n2 == TypeVar.INT){
-						operateurs.push(TypeVar.BOOL);
+						operandes.push(TypeVar.BOOL);
 						return evaluate();
 					}
 				}
