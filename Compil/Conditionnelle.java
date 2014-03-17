cf2bf478dfe2652087bc95b2c10995121276dfda
GPrev <gabriel.prevosto@insa-rennes.fr>
2014-03-17 14:22:52 +0100

    Completion de Conditionnelle.java

--- Compil/Conditionnelle.java
+++ Compil/Conditionnelle.java
@@ -1,5 +1,18 @@
 package Compil;
 
 public class Conditionnelle {
-
+	public static int nbConditionnelles;
+	
+	public Conditionnelle(){
+		nbConditionnelles = 0;
+	}
+	
+	public void verifieTypeExpr()
+	{
+		if(Yaka.expression.dernierOperande() == YakaConstants.BOOLEEN)
+		{
+			System.out.println("Erreur : condition non booléenne");
+			Yaka.nbErreurs++;
+		}
+	}
 }
