843330d13e0375d3fb8f62f6a6fc6d25df1f13c9
Alexandre Audinot <alexandre.audinot@insa-rennes.fr>
2014-03-21 08:46:06 +0100

    iterations

--- Compil/Iteration.java
+++ Compil/Iteration.java
@@ -2,10 +2,16 @@ package Compil;
 
 import java.util.Stack;
 
+
 public class Iteration {
 
 	private static int nbIter;
 	private static Stack<Integer>  niveau;
+	
+	public Iteration() {
+		nbIter = 1;
+		niveau = new Stack<Integer>();
+	}
 	
 	public static int getEtiquette() {
 		return niveau.peek(); 
@@ -18,5 +24,13 @@ public class Iteration {
 	}
 	public void finTantQue() {
 		niveau.pop();
+	}
+	
+	public void verifieTypeExpr()
+	{
+		if(Yaka.expression.dernierOperande() != YakaConstants.BOOLEEN) {
+			System.out.println("Condition non booléenne" + Yaka.afficherLigne());
+			Yaka.nbErreurs++;
+		}
 	}
 }
