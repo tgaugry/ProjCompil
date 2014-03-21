ae5fa56722d135213a0cf32d0003fee50f64759a
Alexandre Audinot <alexandre.audinot@insa-rennes.fr>
2014-03-21 09:41:20 +0100

    IdFonc

---
+++ Compil/IdFonc.java
@@ -0,0 +1,33 @@
+package Compil;
+
+public class IdFonc extends Ident {
+	
+	public IdFonc(int t, int v){
+		super (t,v);
+	}
+
+	@Override
+	public boolean estVar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toYVM() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toYVMAsm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
